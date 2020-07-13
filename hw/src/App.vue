ae9ab8448c83b9a14bc27d7515d56cc4781243ec
仁钟 李 <996955087@qq.com>
2020-07-13 23:56:53 +0800

    Update App.vue - use el-col to create layout Update main.js - install element-UI

--- hw/src/App.vue
+++ hw/src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <el-col :span="4">
+        <div class="grid-head bg-purple"></div>
+        <div class="grid-foot bg-purple-dark"></div>
+      </el-col>
+      <el-col :span="16">
+        <div class="grid-content bg-purple-light"></div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+    </el-row>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
   }
 }
 </script>
@@ -24,5 +36,39 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 700px;
+}
+.grid-foot{
+  border-radius: 4px;
+  min-height: 350px;
+}
+.grid-head{
+  border-radius: 4px;
+  min-height: 350px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
 }
 </style>
